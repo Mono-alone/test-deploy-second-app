d05fed204e0d2af59894b32026e427aa203850b9
Egor Rybakov <rybakovegor.job@gmail.com>
2025-05-06 17:39:44 +0200

    Add sample app with jib plugin

---
+++ src/main/java/com/example/testdeploysecondapp/TestDeploySecondAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.testdeploysecondapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDeploySecondAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestDeploySecondAppApplication.class, args);
+    }
+
+}
